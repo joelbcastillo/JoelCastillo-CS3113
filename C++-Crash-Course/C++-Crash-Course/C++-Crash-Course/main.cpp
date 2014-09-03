5267092c5d13f3b61babfd550706f79fd530b2e5
Joel Castillo <joelbcastillo@gmail.com>
2014-09-03 14:40:34 -0400

    Initial Commit

---
+++ C++-Crash-Course/C++-Crash-Course/C++-Crash-Course/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  C++-Crash-Course
+//
+//  Created by Joel Castillo on 9/3/14.
+//  Copyright (c) 2014 Joel Castillo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

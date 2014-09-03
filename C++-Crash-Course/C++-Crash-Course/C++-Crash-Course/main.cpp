5b0b95a567392d4e935abe14781059eef5a74dd8
Joel Castillo <joelbcastillo@gmail.com>
2014-09-03 15:06:15 -0400

    Part I Assignment I

--- C++-Crash-Course/C++-Crash-Course/C++-Crash-Course/main.cpp
+++ C++-Crash-Course/C++-Crash-Course/C++-Crash-Course/main.cpp
@@ -1,18 +1,29 @@
-//
-//  main.cpp
-//  C++-Crash-Course
-//
-//  Created by Joel Castillo on 9/3/14.
-//  Copyright (c) 2014 Joel Castillo. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[])
-{
+void printString( int n ) {
+    const char* var = "Hello, World!";
+    for( int i = 0; i < n; i++ ) {
+        std::cout << var << std::endl;
+    }
+    std::cout << std::endl;
+    int i = 0;
+    while ( i < n ) {
+        std::cout << var << std::endl;
+        i++;
+    }
+    std::cout << std::endl;
+    i = 0;
+    do {
+        std::cout << var << std::endl;
+        i++;
+    }
+    while (i < n);
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
 }
 
+int main() {
+    const char* var = "Hello, World!";
+    std::cout << var << std::endl;
+    std::cout << std::endl;
+    printString(5);
+}
